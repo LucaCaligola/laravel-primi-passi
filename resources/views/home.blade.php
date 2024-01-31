38ca3fa1816af41361087fa9e2814311f473a99e
LucaCaligola <luca.caligola@gmail.com>
2024-01-31 15:33:05 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Intro</title>
+</head>
+<body>
+    <header>
+        <h1>
+            <?php
+            {{ $title }}                
+            ?>
+        </h1>
+
+        <h2>  
+            <?php
+
+                {{ $secondTitle }}
+
+            ?>
+        </h2>
+    </header>
+    
+</body>
+</html>
